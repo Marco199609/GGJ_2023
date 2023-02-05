cecb00f8adc6af327d0895bbe1b9dd0d831716b4
Moon <nildagomez72256@gmail.com>
2023-02-05 13:14:06 -0300

    incluidos assets y animacion planta humanoide

---
+++ Assets/Scripts/Environment/EnemyPlantHumanoidController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlantHumanoidController : MonoBehaviour
+{
+    private Animator _animator;
+    private PlayerController _playerController;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

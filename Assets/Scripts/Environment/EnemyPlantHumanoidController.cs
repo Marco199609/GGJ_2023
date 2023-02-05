2d7d688501fb54644300111679d983bf8e81476d
Moon <nildagomez72256@gmail.com>
2023-02-05 13:55:45 -0300

    logica de enemigo humanoide

--- Assets/Scripts/Environment/EnemyPlantHumanoidController.cs
+++ Assets/Scripts/Environment/EnemyPlantHumanoidController.cs
@@ -6,15 +6,50 @@ public class EnemyPlantHumanoidController : MonoBehaviour
 {
     private Animator _animator;
     private PlayerController _playerController;
+    private bool attack;
+    private bool run;
     
     void Start()
     {
-        
+        _playerController = FindObjectOfType<PlayerController>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(_playerController.transform.position, transform.position)<10 && Vector3.Distance(_playerController.transform.position, transform.position)>2f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _playerController.transform.position, 1.5f*Time.deltaTime);
+            var dir = _playerController.transform.position;
+            dir.y = 0.5f;
+            transform.LookAt(dir);
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+
+        if (Vector3.Distance(_playerController.transform.position, transform.position) < 2f)
+        {
+            attack = true;
+        }
+        else
+            attack = false;
+        AnimateAttack();
     }
+
+
+    public void AnimateAttack()
+    {
+        if (attack)
+            _animator.SetInteger("State",3);//animacion de ataque se activa
+        else if (!run)
+        {
+            _animator.SetInteger("State",1);//idle se activa
+        }else
+            _animator.SetInteger("State",2);//animacion de run se activa
+    }
+    
 }
--- Assets/Scripts/Environment/Espinas.cs
+++ Assets/Scripts/Environment/Espinas.cs
@@ -5,11 +5,46 @@ using UnityEngine;
 
 public class Espinas : MonoBehaviour
 {
-
-
-    public void ActivateAnimation()
+    private void OnTriggerEnter(Collider other)
     {
         
+        var player = other.GetComponent<PlayerController>();
+        if (player!=null)
+        {
+            StartCoroutine("AnimatedHit",player);
+            var health = player.GetComponent<HealthComponent>();
+            if (health.shield > 0)
+                health.shield--;
+            else
+                health.health--;
+        }
     }
     
+    IEnumerator AnimatedHit(PlayerController playerCollider)
+    {
+        var materials = playerCollider.GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < 4; i++)
+        {
+            foreach (var material in materials)
+            {
+                material.material.color = Color.red;
+            }
+            yield return new WaitForSeconds(.08f);    
+            foreach (var material in materials)
+            {
+                material.material.color = Color.white;
+            }
+            yield return new WaitForSeconds(.08f); 
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        var materials = FindObjectOfType<PlayerController>().GetComponentsInChildren<Renderer>();
+        foreach (var material in materials)
+        {
+            material.material.color = Color.white;
+        }
+    }
 }
